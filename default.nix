02df5ec7064f2e1a46c5b65542cbed566638d875
Abin Simon <mail@meain.io>
2022-01-08 00:25:49 +0530

    Add tojson to the mix

--- default.nix
+++ default.nix
@@ -5,13 +5,18 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
   self = {
+    # personal
+    traffic = callPackage ./packages/traffic {};
     gh-issues-to-rss = callPackage ./packages/gh-issues-to-rss {};
+    tojson = callPackage ./packages/tojson {};
+
+    # external
     fluent-theme = callPackage ./packages/fluent-theme {};
     kmonad = callPackage ./packages/kmonad {};
-    traffic = callPackage ./packages/traffic {};
     notmuch-git = callPackage ./packages/notmuch-git {};
     gnomeExtensions.steal-my-focus = callPackage ./packages/steal-my-focus {};
 
+    # programming
     buf = callPackage ./packages/buf {};
   };
 in
