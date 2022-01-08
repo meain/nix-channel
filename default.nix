4f2b45465ebd7148929fb1fc5b03ca1ebd06f9a3
Abin Simon <mail@meain.io>
2022-01-08 11:59:06 +0530

    Disable upstream updated packages

--- default.nix
+++ default.nix
@@ -5,21 +5,21 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
   self = {
-    # personal
+    ## personal
     traffic = callPackage ./packages/traffic {};
     gh-issues-to-rss = callPackage ./packages/gh-issues-to-rss {};
     tojson = callPackage ./packages/tojson {};
     toffee = callPackage ./packages/toffee {};
     quickserve = callPackage ./packages/quickserve {};
 
-    # external
+    ## external
     fluent-theme = callPackage ./packages/fluent-theme {};
     kmonad = callPackage ./packages/kmonad {};
-    notmuch-git = callPackage ./packages/notmuch-git {};
+    # notmuch-git = callPackage ./packages/notmuch-git {};
     gnomeExtensions.steal-my-focus = callPackage ./packages/steal-my-focus {};
 
-    # programming
-    buf = callPackage ./packages/buf {};
+    ## programming
+    # buf = callPackage ./packages/buf {};
   };
 in
 self
